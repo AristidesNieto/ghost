844a96c346f1f16086b25eeb0b198cee82b816f6
Luis Fernando Flores <a01738347@tec.mx>
2025-10-02 20:02:50 -0600

    Pregunta 2 lista

--- pacman.jl
+++ pacman.jl
@@ -22,7 +22,22 @@ matrix = [
 ]
 
 function agent_step!(agent, model)
-    randomwalk!(agent, model)
+    #ranomwalk(agent, model)
+    moves = [(0,1), (0,-1), (1,0), (-1,0)]
+    posibles = []
+    x, y = agent.pos
+    nrows, ncols = size(matrix)
+    for (dx, dy) in moves
+        nx, ny = x + dx, y + dy
+        
+        if 1 ≤ nx ≤ nrows && 1 ≤ ny ≤ ncols && matrix[nx, ny] == 1
+            push!(posibles, (nx, ny))
+        end
+    end
+    
+    if !isempty(posibles)
+        agent.pos = rand(posibles)
+    end
 end
 
 function initialize_model()
