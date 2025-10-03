bd7201c6d1c812406da6e613566c4ef9a1bad321
Luis Fernando Flores <a01738347@tec.mx>
2025-10-02 19:52:06 -0600

    Mapa visible

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,22 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+
+const matrix = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
 const App = () => {
   const [posX, setPosX] = useState(6);
   const [posY, setPosY] = useState(8);
@@ -17,8 +35,15 @@ const App = () => {
 
   return ( 
     <div>
-      <svg width="800" height="500" style={{backgroundColor: "lightgray"}} xmlns="http://www.w3.org/2000/svg">
-        <image x={255 + 25 * posX} y={9 + 25 * posY} href="ghost.png"/>
+      <svg width="800" height="500" xmlns="http://www.w3.org/2000/svg">
+        {
+          matrix.map((row, rowidx) =>
+            row.map((value, colidx) =>
+              <rect key={`${rowidx}-${colidx}`} x={250 + 25 * rowidx} y={5 + 25 * colidx} width={25} height={25} fill={value === 1 ? "lightgray" : "gray"} />
+            )
+          )
+        }
+        <image x={255 + 25 * posX} y={9 + 25 * posY} href="/ghost.png" width="25" height="25"/>
       </svg>
     </div>
   );
